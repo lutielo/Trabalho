a7faf47c0d87362a94f822cc27fdf0073d81c545
lutielo <lutielo@hotmail.com>
2013-11-04 18:12:45 -0200

    Inserção de action e Menu Itens na tela de menu principal.

--- src/br/unisul/gui/MenuPrincipal.java
+++ src/br/unisul/gui/MenuPrincipal.java
@@ -1,14 +1,22 @@
 package br.unisul.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MenuPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JMenuBar mbMenuBarra;
 	private JMenu jmArquivo;
+	private JMenu jmCadastro;
+	private JMenuItem miCadastrarReceita;
+	private JMenuItem miCadastrarAutor;
+	private JMenuItem miCadastrarIngrediente;
 
 	public MenuPrincipal() {
 		super("Menu Principal");
@@ -25,9 +33,54 @@ public class MenuPrincipal extends JFrame {
 	private void abreTela() {
 		mbMenuBarra = new JMenuBar();
 		this.setJMenuBar(mbMenuBarra);
-		
+
 		jmArquivo = new JMenu("Arquivo");
 		mbMenuBarra.add(jmArquivo);
+
+		jmCadastro = new JMenu("Cadastro");
+		jmArquivo.add(jmCadastro);
+
+		miCadastrarAutor = new JMenuItem("Cadastrar Autor");
+		TrataEventoCadastroAutor trataEventoCadastroAutor = new TrataEventoCadastroAutor();
+		miCadastrarAutor.addActionListener(trataEventoCadastroAutor);
+		jmCadastro.add(miCadastrarAutor);
+
+		miCadastrarIngrediente = new JMenuItem("Cadastrar Ingrediente");
+		TrataEventoCadastroIngrediente trataEventoCadastroIngrediente = new TrataEventoCadastroIngrediente();
+		miCadastrarIngrediente.addActionListener(trataEventoCadastroIngrediente);
+		jmCadastro.add(miCadastrarIngrediente);
+
+		miCadastrarReceita = new JMenuItem("Cadastrar Receita");
+		TrataEventoCadastroReceita trataEventoCadastroReceita = new TrataEventoCadastroReceita();
+		miCadastrarReceita.addActionListener(trataEventoCadastroReceita);
+		jmCadastro.add(miCadastrarReceita);
+	}
+
+	private class TrataEventoCadastroAutor implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			CadastroAutor cadastroAutor = new CadastroAutor();
+			cadastroAutor.setVisible(true);
+		}
+	}
+
+	private class TrataEventoCadastroIngrediente implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			CadastroIngrediente cadastroIngrediente = new CadastroIngrediente();
+			cadastroIngrediente.setVisible(true);
+		}
+	}
+
+	private class TrataEventoCadastroReceita implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			CadastroReceita cadastroReceita = new CadastroReceita();
+			cadastroReceita.setVisible(true);
+		}
 	}
 
 	public static void main(String[] args) {
